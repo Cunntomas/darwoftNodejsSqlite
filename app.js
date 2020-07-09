563d2d98ea7b4d3f3023e9d22503753ae24bb7ab
Tomas <cunn.tomas@gmail.com>
2020-07-09 18:24:03 -0300

    fix moment/sequelize error

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./lib/routes');
+const db = require('./config/database');
 
 
 function initDB() {
-    // Database
-    const db = require('./config/database');
-
     // Test DB
     return db.authenticate()
         .then(() => console.log('Database connected...'))
